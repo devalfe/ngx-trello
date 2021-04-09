cb907a3f184027dceea68ffca67f413ddec077e9
Devalfe <mdt.webmaster@gmail.com>
2021-04-09 10:05:28 -0500

    feat: add navbar, footer and modules Closes #3

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { AuthModule } from './../auth/auth.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule]
+})
+export class CoreModule {}
